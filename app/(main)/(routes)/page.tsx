8186fa16f9f2820ba7c0f3df7414563d7c04cb73
nguyenquochuy <s3818764@rmit.edu.vn>
2024-01-25 14:36:11 +0700

    add code

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,11 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <p className="text-3xl font-bold text-indigo-500">Hello Discord clone</p>
-      <Button>Click me</Button>
+    <div className="h-screen">
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
